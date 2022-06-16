4dd69dd32e5e7e3f35fc41df7e56f86e811b6203
YuliyaMinsk <yuliya.rostova@gmail.com>
2022-06-16 23:35:29 +0300

    feat: run simple server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,19 @@
-import path from 'path';
+import 'dotenv/config';
+import http from 'http';
 
 function run() {
-  console.log('Heya!!!');
+  const host = 'localhost';
+  const port = process.env.SERVER_PORT;
+
+  const requestListener = function (_req: any, res: http.ServerResponse) {
+    res.writeHead(200);
+    res.end('Hello, World!');
+  }
+
+  const server = http.createServer(requestListener);
+  server.listen(Number(port), host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+  });
 }
 
 run();
