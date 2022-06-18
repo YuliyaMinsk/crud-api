4f5bbbcc7c3caf331a06236e6bf7539e0a862362
YuliyaMinsk <yuliya.rostova@gmail.com>
2022-06-18 23:23:51 +0300

    feat: create the main query structure

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,44 @@
 import 'dotenv/config';
 import http from 'http';
 
-function run() {
+function startServer() {
   const host = 'localhost';
-  const port = process.env.SERVER_PORT;
+  const port = Number(process.env.SERVER_PORT);
 
-  const requestListener = function (_req: any, res: http.ServerResponse) {
-    res.writeHead(200);
-    res.end('Hello, World!');
+  const requestListener = function (request: http.IncomingMessage, response: http.ServerResponse) {
+
+    response.setHeader('Content-Type', 'application/json')
+
+    if (request.url?.includes('/api/users')) {
+      switch (request.method) {
+        case 'GET': {
+          break;
+        }
+        case 'POST': {
+          break;
+        }
+        case 'PUT': {
+          break;
+        }
+        case 'DELETE': {
+          break;
+        }
+        default: {
+          response.writeHead(501);
+          response.end('The server does not support the request method');
+        }
+      }
+    } else {
+      response.writeHead(404);
+      response.end('The route do not found');
+    }
   }
 
   const server = http.createServer(requestListener);
-  server.listen(Number(port), host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+
+  server.listen(port, host, () => {
+    console.log(`Server is running on 🚀 http://${host}:${port}`);
   });
 }
 
-run();
+startServer();
