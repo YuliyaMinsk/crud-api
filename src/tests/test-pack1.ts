76a429182e691575d90f6f537ebc2d20fefc8965
YuliyaMinsk <yuliya.rostova@gmail.com>
2022-06-19 19:15:41 +0300

    feat: add first test scenario

---
+++ src/tests/test-pack1.ts
@@ -0,0 +1,98 @@
+import request from 'supertest';
+import assert from 'assert';
+import { validate } from 'uuid';
+
+import startServer from '../server';
+
+describe('Test main requests', () => {
+  const app = startServer();
+
+  const user = {
+    username: 'Boris',
+    age: 25,
+    hobbies: ['painting', 'travel', 'reading']
+  };
+
+  const userToChange = {
+    username: 'Anna',
+    age: 21,
+    hobbies: ['painting']
+  };
+
+  let checkId: string;
+
+  it('Get empty list of users', (done) => {
+    request(app).get('/api/users').expect('Content-Type', /json/).expect(200).expect([]).end(done);
+  });
+  it('Add new user', (done) => {
+    request(app)
+      .post('/api/users')
+      .send(user)
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .expect((response) => {
+        assert.deepEqual(response.body, {
+          id: response.body.id,
+          username: 'Boris',
+          age: 25,
+          hobbies: ['painting', 'travel', 'reading']
+        });
+        assert.ok(validate(response.body.id));
+        checkId = response.body.id;
+      })
+      .end(done);
+  });
+  it('Get new user by id', (done) => {
+    request(app)
+      .get(`/api/users/${checkId}`)
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .expect((response) => {
+        assert.deepEqual(response.body, {
+          id: response.body.id,
+          username: 'Boris',
+          age: 25,
+          hobbies: ['painting', 'travel', 'reading']
+        });
+      })
+      .end(done);
+  });
+  it('Update new user by id', (done) => {
+    request(app)
+      .put(`/api/users/${checkId}`)
+      .send(userToChange)
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .expect((response) => {
+        assert.deepEqual(response.body, {
+          id: response.body.id,
+          username: 'Anna',
+          age: 21,
+          hobbies: ['painting']
+        });
+        assert.ok(validate(response.body.id));
+      })
+      .end(done);
+  });
+  it('Get updated user by id', (done) => {
+    request(app)
+      .get(`/api/users/${checkId}`)
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .expect((response) => {
+        assert.deepEqual(response.body, {
+          id: response.body.id,
+          username: 'Anna',
+          age: 21,
+          hobbies: ['painting']
+        });
+      })
+      .end(done);
+  });
+  it('Delete new user', (done) => {
+    request(app).delete(`/api/users/${checkId}`).expect('Content-Type', /json/).expect(204).end(done);
+  });
+  it('Get empty list of users', (done) => {
+    request(app).get('/api/users').expect('Content-Type', /json/).expect(200).expect([]).end(done);
+  });
+});
